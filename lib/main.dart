fe12020997818b6514dd2e554758e3af43174f92
Mohit-Smarth-Arora <aroram.s2018@gmail.com>
2024-07-13 21:17:53 +0530

    debug true

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: HomePage(),
       theme: Provider.of<ThemeProvider>(context).themeData,
     );
